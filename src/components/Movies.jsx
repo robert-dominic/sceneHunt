0c080e548590416b10e4e01e63a7b2f592dfd427
Robert Dominic <devrobert.codes@gmail.com>
2025-12-23 19:05:54 +0000

    fix: grid display fix to 1 col on mobiles

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -8,7 +8,7 @@ export default function Movies() {
             All Movies
           </h2>
 
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
             {moviesData.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
             ))}
